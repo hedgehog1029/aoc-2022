cf2d45cdb357b4c790b00864cc2266129c7c84be
Henry Le Grys <henry@legrys.info>
2022-12-05 16:18:19 +0000

    Day 5

---
+++ day5-1.exs
@@ -0,0 +1,44 @@
+defmodule AOC.Day do
+  defp read_stack(line) do
+    line
+    |> String.split(",")
+    |> Enum.reverse
+  end
+
+  def read_stacks do
+    File.read!("inputs/day5-stacks.txt")
+    |> String.split("\n")
+    |> Enum.map(&read_stack/1)
+    |> Enum.with_index(fn e, i -> {i + 1, e} end)
+    |> Map.new
+  end
+
+  def run({:move, count, src, dst}, stacks) do
+    ssrc = Map.get(stacks, src)
+    sdst = Map.get(stacks, dst)
+    {taken, rest} = ssrc |> Enum.split(count)
+    to_put = taken |> Enum.reverse
+
+    stacks
+    |> Map.put(src, rest)
+    |> Map.put(dst, to_put ++ sdst)
+  end
+
+  def parse_instruction(ins) do
+    [_h | tail] = Regex.run(~r/move (\d+) from (\d+) to (\d+)/, ins)
+    [count, src, dst] = tail |> Enum.map(&String.to_integer/1)
+    {:move, count, src, dst}
+  end
+
+  def solve(input) do
+    stacks = read_stacks()
+    input
+    |> String.split("\n")
+    |> Enum.map(&parse_instruction/1)
+    |> Enum.reduce(stacks, &run/2)
+    |> Map.values
+    |> Enum.map_join(&List.first/1)
+  end
+end
+
+AOC.solve 5
